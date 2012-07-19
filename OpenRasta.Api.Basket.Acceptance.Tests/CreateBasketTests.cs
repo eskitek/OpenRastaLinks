78d947528c64049cc46d52040a67fbc6a7c32fa0
Emily <Emily.Skitek@7digital.com>
2012-07-19 12:25:56 +0100

    Wrote first failing acceptance test

---
+++ OpenRasta.Api.Basket.Acceptance.Tests/CreateBasketTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using NUnit.Framework;
+
+namespace OpenRasta.Api.Basket.Tests
+{
+	[TestFixture]
+	public class CreateBasketTests
+	{
+		[Test]
+		public void When_I_call_the_create_basket_endpoint_I_get_a_201_Created_response()
+		{
+			var httpRequest = WebRequest.Create("http://localhost/OpenRasta.Api.Basket/Basket");
+			httpRequest.Method = "POST";
+			httpRequest.ContentLength = 0;
+
+			var response = (HttpWebResponse)httpRequest.GetResponse();
+
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+		}
+	}
+}
