ae778e72159758d73c413384be9654d52c04f487
Emily <Emily.Skitek@7digital.com>
2012-07-19 14:24:33 +0100

    Make failing acceptance test pass: When_I_call_the_create_basket_endpoint_I_get_a_201_Created_response

--- OpenRasta.Api.Basket/BasketHandler.cs
+++ OpenRasta.Api.Basket/BasketHandler.cs
@@ -4,6 +4,7 @@ namespace OpenRasta.Api.Basket
 {
 	public class BasketHandler
 	{
+		[HttpOperation(HttpMethod.POST)]
 		public OperationResult Create()
 		{
 			return new OperationResult.Created();
---
+++ OpenRasta.Api.Basket/OpenRasta/ConfigurationSource.cs
@@ -0,0 +1,17 @@
+﻿using OpenRasta.Configuration;
+
+namespace OpenRasta.Api.Basket.OpenRasta
+{
+	public class ConfigurationSource : IConfigurationSource
+	{
+		public void Configure()
+		{
+			using (OpenRastaConfiguration.Manual)
+			{
+				ResourceSpace.Has.ResourcesOfType<object>()
+					.AtUri("/Basket")
+					.HandledBy<BasketHandler>();
+			}
+		}
+	}
+}
