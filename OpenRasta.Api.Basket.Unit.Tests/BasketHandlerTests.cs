7fc35257da9af4ea9cc1c810132e7e7a5c74450f
Emily <Emily.Skitek@7digital.com>
2012-07-19 13:47:17 +0100

    Wrote failing unit test for BasketHandler: Create_returns_Created_operation_result

---
+++ OpenRasta.Api.Basket.Unit.Tests/BasketHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenRasta.Web;
+
+namespace OpenRasta.Api.Basket.Unit.Tests
+{
+	[TestFixture]
+	public class BasketHandlerTests
+	{
+		[Test]
+		public void Create_returns_Created_operation_result()
+		{
+			var basketHandler = new BasketHandler();
+
+			var result = basketHandler.Create();
+
+			Assert.That(result, Is.TypeOf<OperationResult.Created>());
+		}
+	}
+
+	public class BasketHandler
+	{
+		public OperationResult Create()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
