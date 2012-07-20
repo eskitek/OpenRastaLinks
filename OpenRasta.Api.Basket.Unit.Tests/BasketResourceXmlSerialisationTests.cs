8c7838e2aade902c3942159b309badfd3cac3beb
Emily <Emily.Skitek@7digital.com>
2012-07-20 16:54:48 +0100

    Refactored BasketResourceXmlSerialisationTests to remove duplicated code

--- OpenRasta.Api.Basket.Unit.Tests/BasketResourceXmlSerialisationTests.cs
+++ OpenRasta.Api.Basket.Unit.Tests/BasketResourceXmlSerialisationTests.cs
@@ -8,32 +8,29 @@ namespace OpenRasta.Api.Basket.Unit.Tests
 	[TestFixture]
 	public class BasketResourceXmlSerialisationTests
 	{
+		private static XDocument SerialiseBasket(BasketResource basketResource)
+		{
+			var xmlSerialiser = new XmlSerializer(typeof (BasketResource));
+			using (var sw = new StringWriter())
+			{
+				xmlSerialiser.Serialize(sw, basketResource);
+				var serialisedBasket = sw.ToString();
+				return XDocument.Parse(serialisedBasket);
+			}
+		}
+
 		[Test]
 		public void Root_element_is_named_basket()
 		{
-			var xmlSerialiser = new XmlSerializer(typeof(BasketResource));
-			using (var sw = new StringWriter())
-			{
-				var basketResource = new BasketResource();
-				xmlSerialiser.Serialize(sw, basketResource);
-				var serialisedBasket = sw.ToString();
-				var basketXml = XDocument.Parse(serialisedBasket);
-				Assert.That(basketXml.Root.Name.LocalName, Is.EqualTo("basket"), "Root element");
-			}
+			var basketXml = SerialiseBasket(new BasketResource());
+			Assert.That(basketXml.Root.Name.LocalName, Is.EqualTo("basket"));
 		}
 
 		[Test]
 		public void Basket_element_has_an_attribute_named_id()
 		{
-			var xmlSerialiser = new XmlSerializer(typeof(BasketResource));
-			using (var sw = new StringWriter())
-			{
-				var basketResource = new BasketResource { Id = 4 };
-				xmlSerialiser.Serialize(sw, basketResource);
-				var serialisedBasket = sw.ToString();
-				var basketXml = XDocument.Parse(serialisedBasket);
-				Assert.That(basketXml.Root.Attribute("id"), Is.Not.Null, "basket id attribute");
-			}
+			var basketXml = SerialiseBasket(new BasketResource());
+			Assert.That(basketXml.Root.Attribute("id"), Is.Not.Null);
 		}
 	}
 }
