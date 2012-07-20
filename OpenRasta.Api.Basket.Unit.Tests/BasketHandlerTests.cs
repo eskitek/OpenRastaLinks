9bfc91bd30267c6a68b330f0cb58b91e397a7efc
Emily <Emily.Skitek@7digital.com>
2012-07-20 11:26:19 +0100

    Wrote additional BasketHandler unit test: Create_sets_the_redirect_location_on_the_returned_operation_result_to_the_uri_returned_from_CreateGetBasketUri

--- OpenRasta.Api.Basket.Unit.Tests/BasketHandlerTests.cs
+++ OpenRasta.Api.Basket.Unit.Tests/BasketHandlerTests.cs
@@ -18,9 +18,14 @@ namespace OpenRasta.Api.Basket.Unit.Tests
 			_basketHandler = new BasketHandler(_uriCreator);
 		}
 
+		private void StubCreateGetBasketUriToReturn(Uri uri)
+		{
+			_uriCreator.Stub(uc => uc.CreateGetBasketUri(Arg<BasketResource>.Is.Anything)).Return(uri);
+		}
+
 		private void StubCreateGetBasketUriToReturn(string url)
 		{
-			_uriCreator.Stub(uc => uc.CreateGetBasketUri(Arg<BasketResource>.Is.Anything)).Return(new Uri(url));
+			StubCreateGetBasketUriToReturn(new Uri(url));
 		}
 
 		[Test]
@@ -56,6 +61,17 @@ namespace OpenRasta.Api.Basket.Unit.Tests
 			var result = _basketHandler.Create();
 
 			_uriCreator.AssertWasCalled(uc => uc.CreateGetBasketUri(result.ResponseResource as BasketResource));
+		}
+
+		[Test]
+		public void Create_sets_the_redirect_location_on_the_returned_operation_result_to_the_uri_returned_from_CreateGetBasketUri()
+		{
+			var someUri = new Uri("http://www.someurl.com");
+			StubCreateGetBasketUriToReturn(someUri);
+
+			var result = _basketHandler.Create();
+
+			Assert.AreEqual(result.RedirectLocation, someUri);
 		}
 	}
 }
