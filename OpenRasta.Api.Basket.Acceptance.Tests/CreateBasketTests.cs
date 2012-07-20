0bc1d02caa4de286b4ca74669b252488c5398312
Emily <Emily.Skitek@7digital.com>
2012-07-20 10:47:15 +0100

    Refactored Create Basket acceptance tests to remove duplicated code

--- OpenRasta.Api.Basket.Acceptance.Tests/CreateBasketTests.cs
+++ OpenRasta.Api.Basket.Acceptance.Tests/CreateBasketTests.cs
@@ -7,14 +7,27 @@ namespace OpenRasta.Api.Basket.Tests
 	[TestFixture]
 	public class CreateBasketTests
 	{
+		private const string CreateBasketUri = "http://localhost/OpenRasta.Api.Basket/Basket";
+
+		private static HttpWebResponse GetCreateBasketResponse(WebRequest httpRequest)
+		{
+			return (HttpWebResponse)httpRequest.GetResponse();
+		}
+
+		private static WebRequest CreateCreateBasketRequest()
+		{
+			var httpRequest = WebRequest.Create(CreateBasketUri);
+			httpRequest.Method = "POST";
+			httpRequest.ContentLength = 0;
+			return httpRequest;
+		}
+
 		[Test]
 		public void When_I_call_the_create_basket_endpoint_I_get_a_201_Created_response()
 		{
-			var httpRequest = WebRequest.Create("http://localhost/OpenRasta.Api.Basket/Basket");
-			httpRequest.Method = "POST";
-			httpRequest.ContentLength = 0;
+			var request = CreateCreateBasketRequest();
 
-			var response = (HttpWebResponse)httpRequest.GetResponse();
+			var response = GetCreateBasketResponse(request);
 
 			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
 		}
@@ -22,11 +35,9 @@ namespace OpenRasta.Api.Basket.Tests
 		[Test]
 		public void When_I_call_the_create_basket_endpoint_the_response_contains_a_location_header()
 		{
-			var httpRequest = WebRequest.Create("http://localhost/OpenRasta.Api.Basket/Basket");
-			httpRequest.Method = "POST";
-			httpRequest.ContentLength = 0;
+			var request = CreateCreateBasketRequest();
 
-			var response = httpRequest.GetResponse();
+			var response = GetCreateBasketResponse(request);
 
 			Assert.That(response.Headers["Location"], Is.Not.Null);
 		}
@@ -34,11 +45,9 @@ namespace OpenRasta.Api.Basket.Tests
 		[Test]
 		public void When_I_call_the_create_basket_endpoint_the_response_contains_a_response_body()
 		{
-			var httpRequest = WebRequest.Create("http://localhost/OpenRasta.Api.Basket/Basket");
-			httpRequest.Method = "POST";
-			httpRequest.ContentLength = 0;
+			var request = CreateCreateBasketRequest();
 
-			var response = httpRequest.GetResponse();
+			var response = GetCreateBasketResponse(request);
 
 			using (var responseStream = response.GetResponseStream())
 			{
