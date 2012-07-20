14adb620832879391d9ca14e9c06c34517267cda
Emily <Emily.Skitek@7digital.com>
2012-07-20 16:50:09 +0100

    Make test pass

--- OpenRasta.Api.Basket/BasketResource.cs
+++ OpenRasta.Api.Basket/BasketResource.cs
@@ -5,6 +5,7 @@ namespace OpenRasta.Api.Basket
 	[XmlRoot(ElementName = "basket")]
 	public class BasketResource
 	{
+		[XmlAttribute("id")]
 		public int Id { get; set; }
 
 		public LinkResource SelfLink { get; set; }
