c188062157064b380e1e2cb8ca2d6e6721145eaf
Emily <Emily.Skitek@7digital.com>
2012-07-20 11:12:14 +0100

    Refactored BasketHandler unit tests to remove duplicated code.

--- OpenRasta.Api.Basket.Unit.Tests/BasketHandlerTests.cs
+++ OpenRasta.Api.Basket.Unit.Tests/BasketHandlerTests.cs
@@ -7,12 +7,20 @@ namespace OpenRasta.Api.Basket.Unit.Tests
 	[TestFixture]
 	public class BasketHandlerTests
 	{
+		private IUriCreator _uriCreator;
+		private BasketHandler _basketHandler;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_uriCreator = MockRepository.GenerateStub<IUriCreator>();
+			_basketHandler = new BasketHandler(_uriCreator);
+		}
+
 		[Test]
 		public void Create_returns_Created_operation_result()
 		{
-			var basketHandler = new BasketHandler(null);
-
-			var result = basketHandler.Create();
+			var result = _basketHandler.Create();
 
 			Assert.That(result, Is.TypeOf<OperationResult.Created>());
 		}
@@ -20,9 +28,7 @@ namespace OpenRasta.Api.Basket.Unit.Tests
 		[Test]
 		public void Create_sets_the_redirect_location_on_the_returned_operation_result()
 		{
-			var basketHandler = new BasketHandler(null);
-
-			var result = basketHandler.Create();
+			var result = _basketHandler.Create();
 
 			Assert.IsNotNull(result.RedirectLocation);
 		}
@@ -30,9 +36,7 @@ namespace OpenRasta.Api.Basket.Unit.Tests
 		[Test]
 		public void Create_sets_the_response_resource_on_the_returned_operation_result()
 		{
-			var basketHandler = new BasketHandler(null);
-
-			var result = basketHandler.Create();
+			var result = _basketHandler.Create();
 
 			Assert.IsNotNull(result.ResponseResource);
 			Assert.IsInstanceOf<BasketResource>(result.ResponseResource);
